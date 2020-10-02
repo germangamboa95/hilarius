fa046dc1fe0f1d101476ae078332e07f0ea313ce
germangamoa95 <germangamboa95@gmail.com>
2020-10-02 00:46:25 -0400

    only registered users can upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <div class="container">
     <div class="meme-container mx-auto">
+        @auth
         <meme-upload-component></meme-upload-component>
+        @endauth
         <meme-list-component></meme-list-component>
     </div>
 </div>
